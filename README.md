e96b624c3e97600743d31a293d0d8aa25cba69bf
Samuel Viveiros Gomes <samuel.viveiros@gmail.com>
2019-06-03 00:06:17 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# hg-utils README
+
+This is the README for your extension "hg-utils". After writing up a brief description, we recommend including the following sections.
+
+## Features
+
+Shows the name of the current branch on the status bar.
+
+## Requirements
+
+Requires mercurial (hg) installed and configured in environment variable path.
+
+### 0.1.0
+
+Initial release.
+
+-----------------------------------------------------------------------------------------------------------
+
+## Working with Markdown
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,60 @@
+const vscode = require('vscode');
+const util = require('util');
+const path = require('path');
+const childprocess = require('child_process');
+
+const EXTENSION_NAME = 'hello-world';
+const win = vscode.window;
+
+let statusBarItem = null;
+
+function activeTextEditorCallback(event) {
+  statusBarItem.hide();
+
+  let documentPath = path.dirname(event.document.uri.fsPath);
+  let oldWorkingDir = process.cwd();
+
+  try {
+    process.chdir(documentPath);
+
+    childprocess.exec('hg branch', (error, stdout, stderr) => {
+      if (!error) {
+        statusBarItem.text = util.format('Branch: %s', stdout);
+        statusBarItem.show();
+      } else {
+        printError(`hg branch: ${stderr}`);
+      }
+    });
+
+    process.chdir(oldWorkingDir);
+  }
+  catch (e) {
+    printError(`activeTextEditorCallback: ${e.message}`);
+  }
+}
+
+function printLog(msg) {
+  console.log(`[${EXTENSION_NAME}] ${msg}`);
+}
+
+function printError(msg) {
+  console.error(`[${EXTENSION_NAME}] ${msg}`);
+}
+
+function main() {
+  printLog('Extension activated!');
+
+  statusBarItem = win.createStatusBarItem(vscode.StatusBarAlignment.Left, 1000);
+
+  win.onDidChangeActiveTextEditor(
+    event => event && activeTextEditorCallback(event)
+  );
+}
+
+function activate(context) {
+  context.subscriptions.push(main());
+}
+exports.activate = activate;
+
+function deactivate() {}
+exports.deactivate = deactivate;
